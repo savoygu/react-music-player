a70acc9995cc8a0d32db0d9952a1f1a8e95fd4a8
savoygu <savoygu@163.com>
2018-01-11 21:49:57 +0800

    更新：OK

--- app/me/Root.js
+++ app/me/Root.js
@@ -1,31 +1,94 @@
 import React from 'react';
 import Header from './components/header';
 import Player from './pages/player';
-import MUSIC_LIST from './config/musiclist'
+import MusicList from './pages/musiclist';
+import { MUSIC_LIST } from './config/musiclist';
+import { Router, IndexRoute, Link, Route, hashHistory } from 'react-router';
+import Pubsub from 'pubsub-js';
 
-let Root = React.createClass({
+let App = React.createClass({
     getInitialState () {
-        return {}
+        return {
+            musicList: MUSIC_LIST,
+            currentMusicItem: MUSIC_LIST[0]
+        }
+    },
+    playMusic (musicItem) {
+        $('#player').jPlayer('setMedia', {
+            mp3: musicItem.file
+        }).jPlayer('play');
+
+        this.setState({
+            currentMusicItem: musicItem
+        })
+    },
+    playNext (type='next') {
+        let index = this.findMusicIndex(this.state.currentMusicItem);
+        let newIndex = null;
+        let musicListLength = this.state.musicList.length;
+        if (type === 'next') {
+            newIndex = (index + 1) % musicListLength;
+        } else {
+            newIndex = (index - 1 + musicListLength) % musicListLength;
+        }
+
+        this.playMusic(this.state.musicList[newIndex])
+    },
+    findMusicIndex (musicItem) {
+        return this.state.musicList.indexOf(musicItem);
     },
     componentDidMount () {
         $('#player').jPlayer({
-            ready: function () {
-                $(this).jPlayer('setMedia', {
-                    mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
-                }).jPlayer('play');
-            },
             supplied: 'mp3',
             wmode: 'window'
         });
+        this.playMusic(this.state.currentMusicItem);
+        
+        $('#player').bind($.jPlayer.event.ended, (e) => {
+            this.playNext();
+        })
+
+        Pubsub.subscribe('PLAY_MUSIC', (msg, musicItem) => {
+            console.log('====================================');
+            console.log(msg);
+            console.log('====================================');
+            this.playMusic(musicItem);
+        });
+
+        Pubsub.subscribe('DELETE_MUSIC', (msg, musicItem) => {
+            this.setState({
+                musicList: this.state.musicList.filter(item => {
+                    return item !== musicItem;
+                })
+            })
+        });
+    },
+    componentWillUnmount() {
+        Pubsub.unsubscribe('PLAY_MUSIC')
+        Pubsub.unsubscribe('DELETE_MUSIC')
+        $('#player').unbind($.jPlayer.event.ended);
     },
     render () {
         return (
             <div>
                 <Header />
-                <Player />
+                { React.cloneElement(this.props.children, this.state) }
             </div>
         );
     }
+});
+
+let Root = React.createClass({
+   render () {
+       return (
+        <Router history={hashHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Player}></IndexRoute>
+                <Route path="/list" component={MusicList}></Route>
+            </Route>
+        </Router>
+       )
+   } 
 });
 
 export default Root;
--- app/me/pages/player.js
+++ app/me/pages/player.js
@@ -1,36 +1,54 @@
 import React from 'react';
 import Progress from '../components/progress';
 import './player.less';
+import {Link} from 'react-router';
 
 let duration = null
 let Player = React.createClass({
     getInitialState () {
         return {
-            progress: '-'
+			progress: 0,
+			volume: 0,
+			isPlay: true
         }
     },
     componentDidMount () {
         $('#player').bind($.jPlayer.event.timeupdate, (e) => {
             duration = e.jPlayer.status.duration;
             this.setState({
+				volume: e.jPlayer.options.volume * 100,
                 progress: e.jPlayer.status.currentPercentAbsolute
             })
         });
     },
     componentWillUnmount () {
         $('#player').unbind($.jPlayer.event.timeupdate);
-    },
-    progressChangeHandler (progress) {
+	},
+	changeVolumeHandler (progress) {
+		$('#player').jPlayer('volume', progress)
+	},
+    changeProgressHandler (progress) {
         $('#player').jPlayer('play', duration * progress)
-    },
+	},
+	play () {
+		if (this.state.isPlay) {
+			$('#player').jPlayer('pause');
+		} else {
+			$('#player').jPlayer('play');
+		}
+
+		this.setState({
+			isPlay: !this.state.isPlay
+		})
+	},
     render () {
         return (
             <div className="player-page">
-                <h1 className="caption"><Link to="/list">我的私人音乐坊 &gt;</Link></h1>
+                <h1 className="caption"><Link to="/list">我的私人音乐坊</Link></h1>
                 <div className="mt20 row">
                 	<div className="controll-wrapper">
-                		<h2 className="music-title">{this.props.currentMusitItem.title}</h2>
-                		<h3 className="music-artist mt10">{this.props.currentMusitItem.artist}</h3>
+                		<h2 className="music-title">{this.props.currentMusicItem.title}</h2>
+                		<h3 className="music-artist mt10">{this.props.currentMusicItem.artist}</h3>
                 		<div className="row mt20">
                 			<div className="left-time -col-auto">-{this.state.leftTime}</div>
                 			<div className="volume-container">
@@ -45,7 +63,7 @@ let Player = React.createClass({
                 				</div>
                 			</div>
                 		</div>
-                		<div style={{height: 10, lineHeight: '10px'}}>
+                		<div style={{height: 10, lineHeight: '10px', marginTop: '10px'}}>
 			                <Progress
 								progress={this.state.progress}
 								onProgressChange={this.changeProgressHandler}
@@ -64,7 +82,7 @@ let Player = React.createClass({
                 		</div>
                 	</div>
                 	<div className="-col-auto cover">
-                		<img src={this.props.currentMusitItem.cover} alt={this.props.currentMusitItem.title}/>
+                		<img src={this.props.currentMusicItem.cover} alt={this.props.currentMusicItem.title}/>
                 	</div>
                 </div>
             </div>
